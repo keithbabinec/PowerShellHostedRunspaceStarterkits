056acf5304d20d683d01f6f83bab6f14fda9a6e6
Keith Babinec <keith.babinec@gmail.com>
2020-02-11 23:39:14 -0800

    Added default runspace example with script

---
+++ DefaultRunspaceStarter/HostedRunspace.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Threading.Tasks;
+
+namespace DefaultRunspaceStarter
+{
+    /// <summary>
+    /// Contains functionality for executing PowerShell scripts.
+    /// </summary>
+    public class HostedRunspace
+    {
+        /// <summary>
+        /// Runs a PowerShell script with parameters and prints the resulting pipeline objects to the console output. 
+        /// </summary>
+        /// <param name="scriptContents">The script file contents.</param>
+        /// <param name="scriptParameters">A dictionary of parameter names and parameter values.</param>
+        public async Task RunScript(string scriptContents, Dictionary<string, object> scriptParameters)
+        {
+            // create a new hosted PowerShell instance using the default runspace.
+            // wrap in a using statement to ensure resources are cleaned up.
+            
+            using (PowerShell ps = PowerShell.Create())
+            {
+                // specify the script code to run.
+                ps.AddScript(scriptContents);
+
+                // specify the parameters to pass into the script.
+                ps.AddParameters(scriptParameters);
+
+                // execute the script and await the result.
+                var pipelineObjects = await ps.InvokeAsync().ConfigureAwait(false);
+
+                // print the resulting pipeline objects to the console.
+                foreach (var item in pipelineObjects)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            }
+        }
+    }
+}
--- DefaultRunspaceStarter/Program.cs
+++ DefaultRunspaceStarter/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace DefaultRunspaceStarter
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var scriptContents = new StringBuilder();
+            scriptContents.AppendLine("Param($StrParam, $IntParam)");
+            scriptContents.AppendLine("");
+            scriptContents.AppendLine("Write-Output \"Starting script\"");
+            scriptContents.AppendLine("Write-Output \"This is the value from the first param: $StrParam\"");
+            scriptContents.AppendLine("Write-Output \"This is the value from the second param: $IntParam\"");
+            scriptContents.AppendLine("");
+            scriptContents.AppendLine("Write-Output \"Here is some cmdlet output:\"");
+            scriptContents.AppendLine("Get-Date");
+            scriptContents.AppendLine("Get-Childitem | Select-Object -First 5");
+            scriptContents.AppendLine("Get-Service");
+            scriptContents.AppendLine("");
+
+            var scriptParameters = new Dictionary<string, object>()
+            {
+                { "StrParam", "Hello from script" },
+                { "IntParam", 7 }
+            };
+
+            var hosted = new HostedRunspace();
+            await hosted.RunScript(scriptContents.ToString(), scriptParameters);
+
+            Console.Read();
         }
     }
 }
