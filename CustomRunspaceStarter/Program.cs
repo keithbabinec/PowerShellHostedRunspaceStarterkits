10289432c1609dd53ff3125aefbdf67159fcce07
Keith Babinec <keith.babinec@gmail.com>
2020-02-11 22:36:10 -0800

    Added empty vs projects

---
+++ CustomRunspaceStarter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CustomRunspaceStarter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
