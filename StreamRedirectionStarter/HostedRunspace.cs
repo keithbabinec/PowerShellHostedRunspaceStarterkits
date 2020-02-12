575f0b93dec664a4930373b1298a590082e333d6
Keith Babinec <keith.babinec@gmail.com>
2020-02-12 00:29:26 -0800

    Added streams redirection starter example

---
+++ StreamRedirectionStarter/HostedRunspace.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Threading.Tasks;
+
+namespace StreamRedirectionStarter
+{
+    /// <summary>
+    /// Contains functionality for executing PowerShell scripts.
+    /// </summary>
+    public class HostedRunspace
+    {
+        /// <summary>
+        /// Runs a PowerShell script with parameters and prints the resulting pipeline objects and stream messages to the console output. 
+        /// </summary>
+        /// <param name="scriptContents">The script file contents.</param>
+        /// <param name="scriptParameters">A dictionary of parameter names and parameter values.</param>
+        public async Task RunScript(string scriptContents, Dictionary<string, object> scriptParameters)
+        {
+            // create a new hosted PowerShell instance using the default runspace.
+            // wrap in a using statement to ensure resources are cleaned up.
+
+            using (PowerShell ps = PowerShell.Create())
+            {
+                // specify the script code to run.
+                ps.AddScript(scriptContents);
+
+                // specify the parameters to pass into the script.
+                ps.AddParameters(scriptParameters);
+
+                // subscribe to events from some of the streams
+                ps.Streams.Error.DataAdded += Error_DataAdded;
+                ps.Streams.Warning.DataAdded += Warning_DataAdded;
+                ps.Streams.Information.DataAdded += Information_DataAdded;
+
+                // execute the script and await the result.
+                var pipelineObjects = await ps.InvokeAsync().ConfigureAwait(false);
+
+                // print the resulting pipeline objects to the console.
+                
+                Console.WriteLine("----- Pipeline Output below this point -----");
+
+                foreach (var item in pipelineObjects)
+                {
+                    Console.WriteLine(item.BaseObject.ToString());
+                }
+            }
+        }
+
+        private void Information_DataAdded(object sender, DataAddedEventArgs e)
+        {
+            var streamObjectsReceived = sender as PSDataCollection<InformationRecord>;
+            var currentStreamRecord = streamObjectsReceived[e.Index];
+
+            Console.WriteLine($"InfoStreamEvent: {currentStreamRecord.MessageData}");
+        }
+
+        private void Warning_DataAdded(object sender, DataAddedEventArgs e)
+        {
+            var streamObjectsReceived = sender as PSDataCollection<WarningRecord>;
+            var currentStreamRecord = streamObjectsReceived[e.Index];
+
+            Console.WriteLine($"WarningStreamEvent: {currentStreamRecord.Message}");
+        }
+
+        private void Error_DataAdded(object sender, DataAddedEventArgs e)
+        {
+            var streamObjectsReceived = sender as PSDataCollection<ErrorRecord>;
+            var currentStreamRecord = streamObjectsReceived[e.Index];
+
+            Console.WriteLine($"ErrorStreamEvent: {currentStreamRecord.Exception}");
+        }
+    }
+}
