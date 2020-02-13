098fb8a620e55e79134082a247c78d78d8cb2557
Keith Babinec <keith.babinec@gmail.com>
2020-02-12 22:01:19 -0800

    Implemented custom runspace example

---
+++ CustomRunspaceStarter/HostedRunspace.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Threading.Tasks;
+
+namespace CustomRunspaceStarter
+{
+    /// <summary>
+    /// Contains functionality for executing PowerShell scripts.
+    /// </summary>
+    public class HostedRunspace
+    {
+        /// <summary>
+        /// The PowerShell runspace pool.
+        /// </summary>
+        private RunspacePool RsPool { get; set; }
+
+        /// <summary>
+        /// Initialize the runspace pool.
+        /// </summary>
+        /// <param name="minRunspaces"></param>
+        /// <param name="maxRunspaces"></param>
+        public void InitializeRunspaces(int minRunspaces, int maxRunspaces, string[] modulesToLoad)
+        {
+            // create the default session state.
+            // session state can be used to set things like execution policy, language constraints, etc.
+            // optionally load any modules (by name) that were supplied.
+
+            var defaultSessionState = InitialSessionState.CreateDefault();
+            defaultSessionState.ExecutionPolicy = Microsoft.PowerShell.ExecutionPolicy.Unrestricted;
+            
+            foreach (var moduleName in modulesToLoad)
+            {
+                defaultSessionState.ImportPSModule(moduleName);
+            }
+
+            // use the runspace factory to create a pool of runspaces
+            // with a minimum and maximum number of runspaces to maintain.
+            
+            RsPool = RunspaceFactory.CreateRunspacePool(defaultSessionState);
+            RsPool.SetMinRunspaces(minRunspaces);
+            RsPool.SetMaxRunspaces(maxRunspaces);
+
+            // set the pool options for thread use.
+            // we can throw away or re-use the threads depending on the usage scenario.
+
+            RsPool.ThreadOptions = PSThreadOptions.UseNewThread;
+
+            // open the pool. 
+            // this will start by initializing the minimum number of runspaces.
+
+            RsPool.Open();
+        }
+
+        /// <summary>
+        /// Runs a PowerShell script with parameters and prints the resulting pipeline objects to the console output. 
+        /// </summary>
+        /// <param name="scriptContents">The script file contents.</param>
+        /// <param name="scriptParameters">A dictionary of parameter names and parameter values.</param>
+        public async Task RunScript(string scriptContents, Dictionary<string, object> scriptParameters)
+        {
+            if (RsPool == null)
+            {
+                throw new ApplicationException("Runspace Pool must be initialized before calling RunScript().");
+            }
+
+            // create a new hosted PowerShell instance using a custom runspace.
+            // wrap in a using statement to ensure resources are cleaned up.
+
+            using (PowerShell ps = PowerShell.Create())
+            {
+                // use the runspace pool.
+                ps.RunspacePool = RsPool;
+
+                // specify the script code to run.
+                ps.AddScript(scriptContents);
+
+                // specify the parameters to pass into the script.
+                ps.AddParameters(scriptParameters);
+
+                // execute the script and await the result.
+                var pipelineObjects = await ps.InvokeAsync().ConfigureAwait(false);
+
+                // print the resulting pipeline objects to the console.
+                foreach (var item in pipelineObjects)
+                {
+                    Console.WriteLine(item.BaseObject.ToString());
+                }
+            }
+        }
+    }
+}
--- CustomRunspaceStarter/Program.cs
+++ CustomRunspaceStarter/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace CustomRunspaceStarter
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var scriptContents = new StringBuilder();
+            scriptContents.AppendLine("Param($StrParam, $IntParam)");
+            scriptContents.AppendLine("");
+            scriptContents.AppendLine("Write-Output \"Message from inside the running script\"");
+            scriptContents.AppendLine("Write-Output \"This is the value from the first param: $StrParam\"");
+            scriptContents.AppendLine("Write-Output \"This is the value from the second param: $IntParam\"");
+            scriptContents.AppendLine("");
+            scriptContents.AppendLine("Write-Output \"Here are the loaded modules in the script:\"");
+            scriptContents.AppendLine("Get-Module");
+            scriptContents.AppendLine("");
+
+            var scriptParameters = new Dictionary<string, object>()
+            {
+                { "StrParam", "Hello from script" },
+                { "IntParam", 7 }
+            };
+
+            Console.WriteLine("Initializing runspace pool.");
+            
+            // The 'Az' module (bundle) is the Windows Azure PowerShell module that works on both PS 5.1 and PS Core.
+            // For this example to work, the Az module should already be installed.
+            
+            var modulesToLoad = new string[] { "Az.Accounts", "Az.Compute" };
+            
+            var hosted = new HostedRunspace();
+            hosted.InitializeRunspaces(2, 10, modulesToLoad);
+
+            Console.WriteLine("Calling RunScript()");
+            await hosted.RunScript(scriptContents.ToString(), scriptParameters);
+
+            Console.WriteLine("Script execution completed. Press enter key to exit:");
+            Console.Read();
         }
     }
 }
